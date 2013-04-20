849fcc147421df135130130dbe1fd96b5d8bea7b
Grant Butler <butler.grant.j@gmail.com>
2013-04-20 11:31:06 -0400

    Added socket.io API.

---
+++ api.js
@@ -0,0 +1,14 @@
+var fs = require('fs'),
+	path = require('path'),
+	allBlocks = fs.readFileSync(path.resolve(__dirname, 'blocks.json'));
+
+exports.api = function(io, sphero) {
+	io.on('connection', function(socket) {
+		socket.emit('blocks', allBlocks);
+		
+		socket.on('run', function(blocks) {
+			var aRunner = new Runner(blocks, socket, sphero);
+			aRunner.run();
+		});
+	});
+}
