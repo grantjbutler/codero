8c9282098f4fc61220f5fffb7076860634426553
Grant Butler <butler.grant.j@gmail.com>
2013-04-20 10:11:35 -0400

    Load blocks from json file. Added stubs for endpoints.

--- routes/api.js
+++ routes/api.js
@@ -1,121 +1,17 @@
+var fs = require('fs'),
+	path = require('path',)
+	blocks = fs.readFileSync(path.resolve(__dirname, 'blocks.json'));
+
 exports.blocks = function(req, res) {
-	res.json([
-		{
-			name: "Go Forward",
-			id: "go-forward",
-			type: "command",
-			label: "Method"
-			panels: [
-				{
-					title: "Time",
-					params: [
-						label: "Seconds"
-						name: "time",
-						type: "field",
-					]
-				},
-				{
-					title: "Rotations",
-					params: [
-						label: "Rotations",
-						name: "rotation",
-						type: "field"
-					]
-				},
-				{
-					title: "Indefinitely",
-				}
-			]
-		},
-		{
-			name: "Go Backward",
-			id: "go-backward",
-			type: "command",
-			label: "Method"
-			panels: [
-				{
-					title: "Time",
-					params: [
-						label: "Seconds"
-						name: "time",
-						type: "field",
-					]
-				},
-				{
-					title: "Rotations",
-					params: [
-						label: "Rotations",
-						name: "rotation",
-						type: "field"
-					]
-				},
-				{
-					title: "Indefinitely",
-				}
-			]
-		},
-		{
-			name: "Stop",
-			id: "stop",
-			type: "command"
-		},
-		{
-			name: "Turn Left",
-			id: "turn-left",
-			type: "command",
-			label: "Method"
-			panels: [
-				{
-					title: "Time",
-					params: [
-						label: "Seconds"
-						name: "time",
-						type: "field",
-					]
-				},
-				{
-					title: "Heading",
-					params: [
-						label: "Degrees",
-						name: "heading",
-						type: "field"
-					]
-				},
-				{
-					title: "Indefinitely",
-				}
-			]
-		},
-		{
-			name: "Turn Right",
-			id: "turn-right",
-			type: "command",
-			label: "Method"
-			panels: [
-				{
-					title: "Time",
-					params: [
-						label: "Seconds"
-						name: "time",
-						type: "field",
-					]
-				},
-				{
-					title: "Heading",
-					params: [
-						label: "Degrees",
-						name: "heading",
-						type: "field"
-					]
-				},
-				{
-					title: "Indefinitely",
-				}
-			]
-		}
-	]);
+	res.json(blocks);
 };
 
 exports.run = function(req, res, sphero) {
+	var blocks = req.body;
+	
+	
+};
+
+exports.execute = function(req, res, sphero) {
 	
 };
