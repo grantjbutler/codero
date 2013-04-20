cec8d2d9311d44b55cb62a2c949a8ebde6283cb1
Grant Butler <butler.grant.j@gmail.com>
2013-04-20 10:11:48 -0400

    Added stub endpoints.

--- app.js
+++ app.js
@@ -5,31 +5,44 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , api = require('./routes/api')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , Sphero = require('Sphero').Sphero;
 
-var app = express();
+var sphero = new Sphero();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+sphero.on('connect', function(ball) {
+	var app = express();
+	
+	app.configure(function(){
+	  app.set('port', process.env.PORT || 3000);
+	  app.set('views', __dirname + '/views');
+	  app.set('view engine', 'jade');
+	  app.use(express.favicon());
+	  app.use(express.logger('dev'));
+	  app.use(express.bodyParser());
+	  app.use(express.methodOverride());
+	  app.use(app.router);
+	  app.use(express.static(path.join(__dirname, 'public')));
+	});
+	
+	app.configure('development', function(){
+	  app.use(express.errorHandler());
+	});
+	
+	app.get('/', routes.index);
+	app.get('/api/blocks', api.blocks);
+	app.post('/api/run', function(req, res) {
+		api.run(req, res, ball);
+	});
+	app.post('/api/execute', function(req, res) {
+		api.execute(req, res, sphero);
+	})
+	
+	http.createServer(app).listen(app.get('port'), function(){
+	  console.log("Express server listening on port " + app.get('port'));
+	});
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+sphero.connect();
