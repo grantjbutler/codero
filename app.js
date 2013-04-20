92528ebbfaf7bd46daa216e518cb725797f1c207
Grant Butler <butler.grant.j@gmail.com>
2013-04-20 12:15:34 -0400

    Updates to allow the server to start even if a Sphero isn't connected.

--- app.js
+++ app.js
@@ -12,33 +12,35 @@ var express = require('express')
 
 var sphero = new Sphero();
 
+var app = express();
+	
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+var server = http.createServer(app);
+socketIO.listen(server);
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
 sphero.on('connect', function(ball) {
-	var app = express();
-	
-	app.configure(function(){
-	  app.set('port', process.env.PORT || 3000);
-	  app.set('views', __dirname + '/views');
-	  app.set('view engine', 'ejs');
-	  app.use(express.favicon());
-	  app.use(express.logger('dev'));
-	  app.use(express.bodyParser());
-	  app.use(express.methodOverride());
-	  app.use(app.router);
-	  app.use(express.static(path.join(__dirname, 'public')));
-	});
-	
-	app.configure('development', function(){
-	  app.use(express.errorHandler());
-	});
-	
-	app.get('/', routes.index);
-	
-	var server = http.createServer(app);
-	socketIO.listen(server);
 	require('./api').api(socketIO, sphero);
-	server.listen(app.get('port'), function(){
-	  console.log("Express server listening on port " + app.get('port'));
-	});
 });
 
 sphero.connect();
